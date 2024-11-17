6ef66025d38b3affb429a7ba971b1139d6a7e018
TobiasBlankJohansson <tobias@annito.se>
2024-11-17 20:29:15 +0100

    feat:add header and start to form in meal week

--- frontend/src/components/Meal/Meal.tsx
+++ frontend/src/components/Meal/Meal.tsx
@@ -7,7 +7,7 @@ export function Meal() {
       <Navbar></Navbar>
       <main className="mx-20 mt-5">
         <div className="flex w-full flex-col lg:flex-row">
-          <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
+          <div className="card bg-base-300 rounded-box h-32 flex-grow">
             <MealWeek></MealWeek>
           </div>
           <div className="divider lg:divider-horizontal">OR</div>
--- frontend/src/components/Meal/MealWeek.tsx
+++ frontend/src/components/Meal/MealWeek.tsx
@@ -1,3 +1,23 @@
 export function MealWeek() {
-  return <></>;
+  return (
+    <section>
+      <header className="flex flex-col items-center">
+        <h2 className="my-5 text-xl">Weeks</h2>
+        <hr className="w-2/4"></hr>
+      </header>
+      <form>
+        <section>
+          <label>
+            Monday
+            <input
+              type="text"
+              placeholder="Count"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <button className="btn">Select</button>
+          </label>
+        </section>
+      </form>
+    </section>
+  );
 }
