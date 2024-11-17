1005b29f87618a6b7658b71a151d690adcfef27b
TobiasBlankJohansson <tobias@annito.se>
2024-11-17 20:59:02 +0100

    feat:add styling for elements

--- frontend/src/components/Meal/MealWeek.tsx
+++ frontend/src/components/Meal/MealWeek.tsx
@@ -14,15 +14,15 @@ export function MealWeek() {
   return (
     <section>
       <header className="flex flex-col items-center">
-        <h2 className="my-5 text-xl">Weeks</h2>
+        <h2 className="my-5 text-4xl text-primary font-bold ">Weeks</h2>
         <hr className="w-2/4"></hr>
       </header>
-      <form>
+      <form className="flex flex-col items-center">
         {week.map((day) => {
           return <WeekFormItem day={day}></WeekFormItem>;
         })}
+        <button className="btn btn-secondary mt-5 w-1/2 h-16">Button</button>
       </form>
-      <button className="btn">Button</button>
     </section>
   );
 }
--- frontend/src/components/Meal/WeekFormItem.tsx
+++ frontend/src/components/Meal/WeekFormItem.tsx
@@ -4,16 +4,14 @@ type weekFormItemProp = {
 
 export function WeekFormItem({ day }: weekFormItemProp) {
   return (
-    <section>
-      <label>
-        {day}
-        <input
-          type="text"
-          placeholder="Count"
-          className="input input-bordered w-full max-w-xs"
-        />
-        <button className="btn">Select</button>
-      </label>
+    <section className="mt-5 grid grid-cols-3">
+      <label className="">{day}</label>
+      <input
+        type="text"
+        placeholder="Count"
+        className="input input-bordered w-full max-w-xs"
+      />
+      <button className="btn">Select</button>
     </section>
   );
 }
