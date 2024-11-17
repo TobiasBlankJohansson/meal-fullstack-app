a80300a2b6e218e50313a18f77fc79d7c7c9ba46
TobiasBlankJohansson <tobias@annito.se>
2024-11-17 18:58:46 +0100

    feat:add boildercode for navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export function Navbar() {
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-primary text-primary-content">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
@@ -11,19 +11,21 @@ export function Navbar() {
               <a>Link</a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a>Link</a>
             </li>
           </ul>
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,5 +4,22 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui'),],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FF7043",
+          secondary: "#FFA726",
+          accent: "#8BC34A",
+          neutral: "#BDBDBD",
+          "base-100": "#FFFFFF",
+          info: "#29B6F6",
+          success: "#66BB6A",
+          warning: "#FFCA28",
+          error: "#E53935",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
