2e955b96cbda2cf0f9a379f27a5abda2a6f9a0c5
TobiasBlankJohansson <Tobias@annito.se>
2024-11-18 11:15:07 +0100

    feat:add logic to hide button if no more recepis exist

--- frontend/src/components/Meal/Recipe/MealRecipe.tsx
+++ frontend/src/components/Meal/Recipe/MealRecipe.tsx
@@ -130,20 +130,24 @@ export function MealRecipe({ setMeal, mealSelect }: mealRecipeProp) {
               })}
             </ol>
           </main>
-          <footer className="flex justify-center items-center">
-            <button
-              className="btn btn-ghost text-2xl font-bold"
-              onClick={() => SetRecipeCount((prev) => prev - 1)}
-            >
-              {"<"}
-            </button>
-            <p className="m-5 text-2xl font-bold">{recipeCount + 1}</p>
-            <button
-              className="btn btn-ghost text-2xl font-bold"
-              onClick={() => SetRecipeCount((prev) => prev + 1)}
-            >
-              {">"}
-            </button>
+          <footer className="grid grid-cols-3 justify-center items-center">
+            {recipeCount != 0 && (
+              <button
+                className="btn btn-ghost text-2xl font-bold"
+                onClick={() => SetRecipeCount((prev) => prev - 1)}
+              >
+                {"<"}
+              </button>
+            )}
+            <p className="m-5 text-2xl font-bold flex justify-center col-start-2">{recipeCount + 1}</p>
+            {recipeCount * 10 < mealMock.length - 10 && (
+              <button
+                className="btn btn-ghost text-2xl font-bold"
+                onClick={() => SetRecipeCount((prev) => prev + 1)}
+              >
+                {">"}
+              </button>
+            )}
           </footer>
         </div>
       </dialog>
