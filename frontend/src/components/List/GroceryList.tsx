329b21bcf098d986bf2f14266951a7254f4f4e29
TobiasBlankJohansson <Tobias@annito.se>
2024-11-18 15:38:36 +0100

    feat:add header to grocerylist

--- frontend/src/components/List/GroceryList.tsx
+++ frontend/src/components/List/GroceryList.tsx
@@ -1,7 +1,10 @@
 export function GroceryList() {
   return (
     <section>
-      <></>
+      <header className="flex flex-col items-center">
+        <h2 className="mb-2 text-4xl text-primary font-bold ">To buy</h2>
+        <hr className="w-2/4"></hr>
+      </header>
     </section>
   );
 }
--- frontend/src/components/List/List.tsx
+++ frontend/src/components/List/List.tsx
@@ -2,7 +2,7 @@ import { GroceryList } from "./GroceryList";
 
 export function List() {
   return (
-    <main>
+    <main className="mx-20 mt-5">
       <GroceryList></GroceryList>
     </main>
   );
