40c64129d91ade0b44a09e0043dcb215ffb22a08
TobiasBlankJohansson <Tobias@annito.se>
2024-11-19 13:34:27 +0100

    feat:add RecipeRequestDto and it's logic

--- backend/src/main/java/salt/tobias/meal/recipe/controller/RecipeRequestDto.java
+++ backend/src/main/java/salt/tobias/meal/recipe/controller/RecipeRequestDto.java
@@ -1,4 +1,6 @@
 package salt.tobias.meal.recipe.controller;
 
-public record RecipeRequestDto() {
+public record RecipeRequestDto(Number[] serving,RecipeItemDto[] recipeItemDto) {
+    private record RecipeItemDto(String title, String ingredients, String instructions, String servings) {
+    }
 }
