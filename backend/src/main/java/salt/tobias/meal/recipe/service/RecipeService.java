e574c8638c1feeb98be7d40686d4fe6b6e9fc4f5
TobiasBlankJohansson <Tobias@annito.se>
2024-11-19 16:15:43 +0100

    feat:add getRecipes in RecipeService

--- backend/src/main/java/salt/tobias/meal/recipe/service/RecipeService.java
+++ backend/src/main/java/salt/tobias/meal/recipe/service/RecipeService.java
@@ -2,8 +2,14 @@ package salt.tobias.meal.recipe.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RecipeService {
+
+    public List<String> getRecipes(){
+        return null;
+    }
 
     public boolean saveRecipes(double[] Servings,RecipeItem[] recipes){
         return true;
