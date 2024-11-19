a8d332c0a362a57d1f082c01b850b44aa624e41b
TobiasBlankJohansson <Tobias@annito.se>
2024-11-19 10:34:33 +0100

    feat:add tailwind styling for gorcerylist

--- frontend/src/components/List/GroceryList.tsx
+++ frontend/src/components/List/GroceryList.tsx
@@ -13,13 +13,13 @@ export function GroceryList() {
   }, []);
 
   return (
-    <section>
-      <header className="flex flex-col items-center">
-        <h2 className="mb-2 text-4xl text-primary font-bold ">To buy</h2>
-        <hr className="w-2/4"></hr>
+    <section className="bg-gray-50 py-8">
+      <header className="flex flex-col items-center mb-8">
+        <h2 className="mb-2 text-4xl text-primary font-bold">To buy</h2>
+        <hr className="w-2/4 border-t-2 border-primary" />
       </header>
       <main className="flex justify-center">
-        <ol className="w-2/5">
+        <ol className="w-2/5 text-lg text-gray-700 space-y-2 list-inside">
           {list.map((item) => (
             <li className="list-disc">{item}</li>
           ))}
