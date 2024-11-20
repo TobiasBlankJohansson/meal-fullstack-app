371f3e718099169868b0fbb711672d99f4ff2b36
TobiasBlankJohansson <Tobias@annito.se>
2024-11-20 15:33:21 +0100

    feat:add RecipeSearch

---
+++ backend/src/main/java/salt/tobias/meal/recipeApi/model/RecipeSearch.java
@@ -0,0 +1,19 @@
+package salt.tobias.meal.recipeApi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recipe_search")
+public class RecipeSearch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name="recipe_id")
+    private Recipe recipe;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "search_id")
+    private Search search;
+}
