af21350158ade2793303edba94b017b75f27cd97
TobiasBlankJohansson <Tobias@annito.se>
2024-11-20 16:21:14 +0100

    feat:add return as recipe and return all when searchWord is

--- backend/src/main/java/salt/tobias/meal/recipeApi/service/RecipeRepository.java
+++ backend/src/main/java/salt/tobias/meal/recipeApi/service/RecipeRepository.java
@@ -1,7 +1,7 @@
 package salt.tobias.meal.recipeApi.service;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import salt.tobias.meal.recipeApi.model.Recipe;
 
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+public interface RecipeRepository extends ListCrudRepository<Recipe, Long> {
 }
--- backend/src/main/java/salt/tobias/meal/recipeApi/service/SearchRepository.java
+++ backend/src/main/java/salt/tobias/meal/recipeApi/service/SearchRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.ListCrudRepository;
 import salt.tobias.meal.recipeApi.model.Search;
 
 public interface SearchRepository extends ListCrudRepository<Search,Long> {
+    boolean existsByWordAndPage(String word, long page);
 }
