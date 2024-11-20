224a290faebf71898f072adf73a6bc4f4e8f47fa
TobiasBlankJohansson <Tobias@annito.se>
2024-11-20 10:55:51 +0100

    feat:add recipeApiController

---
+++ backend/src/main/java/salt/tobias/meal/recipeApi/controller/recipeApiController.java
@@ -0,0 +1,13 @@
+package salt.tobias.meal.recipeApi.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/api/recipes")
+public class recipeApiController {
+
+
+
+}
