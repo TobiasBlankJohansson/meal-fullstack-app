b7cb2b0b06e0769e4f3fc4f2a821289e37a94123
TobiasBlankJohansson <Tobias@annito.se>
2024-11-20 13:11:19 +0100

    feat:add RecipeSearch record with searchWordPage and recipes

--- backend/src/main/java/salt/tobias/meal/recipeApi/service/recipeApiService.java
+++ backend/src/main/java/salt/tobias/meal/recipeApi/service/recipeApiService.java
@@ -9,6 +9,8 @@ public class recipeApiService {
 
     private List<RecipeItem> recipes = new ArrayList<>();
 
+    private record RecipeSearch(String searchWordPage, List<RecipeItem> recipes) {}
+
     public List<RecipeItem> getRecipe(String searchWord, int page) {
         if(searchWord.equals("random")){
             return recipes;
