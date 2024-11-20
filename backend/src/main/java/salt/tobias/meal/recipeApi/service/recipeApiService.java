d6119930a68507fb7d2819ca34e0cd772f38c5ed
TobiasBlankJohansson <Tobias@annito.se>
2024-11-20 13:20:39 +0100

    feat:add mock fetch in getRecipe and return a list of recipe

--- backend/src/main/java/salt/tobias/meal/recipeApi/service/recipeApiService.java
+++ backend/src/main/java/salt/tobias/meal/recipeApi/service/recipeApiService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class recipeApiService {
 
     private List<RecipeItem> recipes = new ArrayList<>();
+    private List<RecipeSearch> searches = new ArrayList<>();
 
     private record RecipeSearch(String searchWordPage, List<RecipeItem> recipes) {}
 
@@ -15,7 +16,19 @@ public class recipeApiService {
         if(searchWord.equals("random")){
             return recipes;
         }
+        if(searches.contains(searchWord+page)){
+            return recipes;
+        }
+        var recipePage = fetchRecipes(searchWord, page);
+        var recipeNextPage = fetchRecipes(searchWord, page+1);
 
+        searches.add(new RecipeSearch(searchWord+page, recipePage));
+        searches.add(new RecipeSearch(searchWord+(page+1), recipeNextPage));
+        recipePage.addAll(recipeNextPage);
+        return recipePage;
+    }
+
+    private List<RecipeItem> fetchRecipes(String searchWord, int page) {
     }
 
 }
