c5b833b95003077ca8c6c84c67ff3afd060cbdaf
TobiasBlankJohansson <Tobias@annito.se>
2024-11-21 09:20:18 +0100

    feat:add weekRepo in RecipeService

--- backend/src/main/java/salt/tobias/meal/recipe/service/RecipeService.java
+++ backend/src/main/java/salt/tobias/meal/recipe/service/RecipeService.java
@@ -1,6 +1,7 @@
 package salt.tobias.meal.recipe.service;
 
 import org.springframework.stereotype.Service;
+import salt.tobias.meal.recipeApi.model.Recipe;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,6 +9,8 @@ import java.util.List;
 
 @Service
 public class RecipeService {
+
+    private final WeekRepository weekRepo;
 
     private List<MealServings> recipes = new ArrayList<>(Arrays.asList(
             new MealServings(0, new RecipeItem("select", "", "", "0")),
@@ -19,11 +22,15 @@ public class RecipeService {
             new MealServings(0, new RecipeItem("select", "", "", "0"))
         ));
 
+    public RecipeService(WeekRepository weekRepo) {
+        this.weekRepo = weekRepo;
+    }
+
     public List<MealServings> getRecipes(){
         return recipes;
     }
 
-    public boolean saveRecipes(double[] servings, RecipeItem[] recipes){
+    public boolean saveRecipes(double[] servings, Recipe[] recipes, long id){
 
 
 
