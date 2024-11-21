1763c9b8f0007a058cb78ac23bf89e0ce8064dad
TobiasBlankJohansson <Tobias@annito.se>
2024-11-21 16:50:32 +0100

    feat:add  clerk

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")!).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <App />
+  </ClerkProvider>
 );
